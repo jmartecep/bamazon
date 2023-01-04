68389e8f9354d945fd31fb939ce7167ca9a5aa18
Jamie Pena <jamie.m.pena@gmail.com>
2023-01-03 19:37:11 -0500

    Video-05-Show-List-of-Products

--- README.md
+++ README.md
@@ -6,3 +6,8 @@
 2. Install VSCODE Tools
 3. Create React App
 4. Create Git Repository
+5. Show List of Products
+   1. create products array
+   2. add product images
+   3. render products
+   4. style products
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,36 @@
+import data from './data';
+
 function App() {
   return (
     <div>
       <header>
         <a href="/">bamazon</a>
       </header>
-      <main>List Products</main>
+      <main>
+        <h1>Featured Products</h1>
+        <div className="products">
+          {/*Use the MAP function and convert code to JSX */}
+          {data.products.map((product) => (
+            /* 
+              Make a KEY prop to avoid errors.
+            */
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="product-info">
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p>
+                  <strong>${product.price}</strong>
+                </p>
+                <button>Add to cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
