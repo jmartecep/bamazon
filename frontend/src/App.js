9a8a61ae469b565a6326a309d9dbb3b904cbd24b
Jamie Pena <jamie.m.pena@gmail.com>
2023-01-03 20:04:04 -0500

    Video-06-Add-Page-Routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,37 +1,22 @@
-import data from './data';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
   return (
-    <div>
-      <header>
-        <a href="/">bamazon</a>
-      </header>
-      <main>
-        <h1>Featured Products</h1>
-        <div className="products">
-          {/*Use the MAP function and convert code to JSX */}
-          {data.products.map((product) => (
-            /* 
-              Make a KEY prop to avoid errors.
-            */
-            <div className="product" key={product.slug}>
-              <a href={`/product/${product.slug}`}>
-                <img src={product.image} alt={product.name} />
-              </a>
-              <div className="product-info">
-                <a href={`/product/${product.slug}`}>
-                  <p>{product.name}</p>
-                </a>
-                <p>
-                  <strong>${product.price}</strong>
-                </p>
-                <button>Add to cart</button>
-              </div>
-            </div>
-          ))}
-        </div>
-      </main>
-    </div>
+    <BrowserRouter>
+      <div>
+        <header>
+          <Link to="/">bamazon</Link>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/product/:slug" element={<ProductScreen />} />
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,33 @@
+import { Link } from 'react-router-dom';
+import data from '../data';
+
+function HomeScreen() {
+  return (
+    <div>
+      <h1>Featured Products</h1>
+      <div className="products">
+        {/*Use the MAP function and convert code to JSX */}
+        {data.products.map((product) => (
+          /* 
+              Make a KEY prop to avoid errors.
+            */
+          <div className="product" key={product.slug}>
+            <Link to={`/product/${product.slug}`}>
+              <img src={product.image} alt={product.name} />
+            </Link>
+            <div className="product-info">
+              <Link to={`/product/${product.slug}`}>
+                <p>{product.name}</p>
+              </Link>
+              <p>
+                <strong>${product.price}</strong>
+              </p>
+              <button>Add to cart</button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+export default HomeScreen;
