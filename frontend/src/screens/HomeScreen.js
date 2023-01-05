ff90a0db5e856f7ddbcd9eadbe78bf4af91df358
Jamie Pena <jamie.m.pena@gmail.com>
2023-01-04 22:43:36 -0500

    Video-08-Fetch-Products-From-Backend

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,13 +1,25 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import data from '../data';
+import axios from 'axios';
+//import data from '../data'; data is no longer needed
 
 function HomeScreen() {
+  /* Creating State Hooks */
+  const [products, setProducts] = useState([]);
+  /* UseEffect takes 2 parameters */
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get('/api/products');
+      setProducts(result.data);
+    };
+    fetchData(); /* to stop the function */
+  }, []);
   return (
     <div>
       <h1>Featured Products</h1>
       <div className="products">
         {/*Use the MAP function and convert code to JSX */}
-        {data.products.map((product) => (
+        {products.map((product) => (
           /* 
               Make a KEY prop to avoid errors.
             */
